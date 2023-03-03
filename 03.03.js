a4f6f0857f76191ab4fe91336a8c3b5da305e05c
Paziu <113362954+Paziu050@users.noreply.github.com>
2023-03-03 12:09:44 +0100

    Create 03.03.js

---
+++ 03.03.js
@@ -0,0 +1,14 @@
+document.querySelector("#pierwszy").addEventListener("click", function (){
+    const przyc = document.querySelector("#cosik");
+    console.log(przyc.dataset.x)
+    
+    if(cosik.style.display == 'none')
+    {
+        cosik.style.display = 'block';
+    }
+    else
+    {
+        cosik.style.display = 'none';
+    }
+})
+//
